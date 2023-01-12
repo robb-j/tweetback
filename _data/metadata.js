231779283469dffd267496dd1d14868a73a0c38f
Rob Anderson <robanderson@hey.com>
2023-01-12 22:54:13 +0000

    run build

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,9 +1,11 @@
 let data = {
-	username: "eleven_ty", // No leading @ here
-	homeLabel: "11ty.dev",
-	homeUrl: "https://www.11ty.dev/",
+  username: "robbb_J", // No leading @ here
+  homeLabel: "r0b.io",
+  homeUrl: "https://www.r0b.io/",
 };
 
-data.avatar = `https://v1.indieweb-avatar.11ty.dev/${encodeURIComponent(data.homeUrl)}/`;
+data.avatar = `https://v1.indieweb-avatar.11ty.dev/${encodeURIComponent(
+  data.homeUrl
+)}/`;
 
 module.exports = data;
